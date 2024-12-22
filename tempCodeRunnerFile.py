6a331b858f57b65985011ae87b2c075811324b1c
Nikhilkashyap07 <nikhillashyap07@gmail.com>
2024-12-22 14:16:40 +0530

    This is my first commit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,22 @@
+def data_time():
+#     time = datetime.datetime.now()
+#     hr = time.strftime("%I")
+#     mi = time.strftime("%M")
+#     sec = time.strftime("%S")
+#     am = time.strftime("%p")
+
+#     date = time.strftime("%d")
+#     month = time.strftime("%m")
+#     year = time.strftime("%Y")
+#     day = time.strftime("%a")
+
+#     lab_date.config(text=date)
+#     lab_month.config(text=month)
+#     lab_year.config(text=year)
+#     lab_day.config(text=day)
+
+#     lab_hr.config(text=hr)
+#     lab_min.config(text=mi)
+#     lab_sec.config(text=sec)
+#     lab_am.config(text=am)
+#     lab_hr.after(200,data_time)
